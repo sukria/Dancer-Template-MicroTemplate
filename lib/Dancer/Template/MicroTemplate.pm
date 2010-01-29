ced886f2f30e450d38148c7e6341560b5b84bf16
Alexis Sukrieh <sukria@sukria.net>
2010-01-29 17:40:22 +0100

    README update, added lib/Dancer/Template/MicroTemplate

--- lib/Dancer/Template/MicroTemplate.pm
+++ lib/Dancer/Template/MicroTemplate.pm
@@ -1,110 +1,75 @@
 package Dancer::Template::MicroTemplate;
 
-use warnings;
 use strict;
+use warnings;
+use Dancer::ModuleLoader;
+use Dancer::FileUtils 'path';
+
+use base 'Dancer::Template::Abstract';
+
+my $_engine;
+
+sub init {
+    my $self = shift;
+    die "Text::MicroTemplate is needed by Dancer::Template::MicroTemplate"
+      unless Dancer::ModuleLoader->load('Text::MicroTemplate::File');
+
+    my $mt_cfg = {
+        use_cache  => 0,
+        tag_start  => '<%',
+        tag_end    => '%>',
+        line_start => '%',
+    };
+    my $path = path($self->{settings}{appdir}, 'views');
+    $mt_cfg->{include_path} = [$path]
+      if $self->{settings} && $self->{settings}{appdir};
+
+    $_engine = Text::MicroTemplate::File->new(%$mt_cfg);
+}
+
+sub render($$$) {
+    my ($self, $template, $tokens) = @_;
+
+    die "'$template' is not a regular file"
+      if ref($template) || (!-f $template);
+
+    my $content = "";
+    $content = $_engine->render_file($template, $tokens)->as_string;
+    return $content;
+}
+
+1;
+__END__
+
+=pod
 
 =head1 NAME
 
-Dancer::Template::MicroTemplate - The great new Dancer::Template::MicroTemplate!
+Dancer::Template::MicroTemplate - MicroTemplate wrapper for Dancer
 
-=head1 VERSION
+=head1 DESCRIPTION
 
-Version 0.01
+This class is an interface between Dancer's template engine abstraction layer
+and the L<Text::MicroTemplate> module.
 
-=cut
+In order to use this engine, set the following setting as the following:
 
-our $VERSION = '0.01';
+    template: micro_template
 
+This can be done in your config.yml file or directly in your app code with the
+B<set> keyword.
 
-=head1 SYNOPSIS
+=head1 SEE ALSO
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Dancer::Template::MicroTemplate;
-
-    my $foo = Dancer::Template::MicroTemplate->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+L<Dancer>, L<Text::MicroTemplate>
 
 =head1 AUTHOR
 
-Franck Cuny, C<< <franck at lumberjaph.net> >>
+This module has been written by Franck Cuny
 
-=head1 BUGS
+=head1 LICENSE
 
-Please report any bugs or feature requests to C<bug-dancer-template-microtemplate at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer-Template-MicroTemplate>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Dancer::Template::MicroTemplate
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Template-MicroTemplate>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dancer-Template-MicroTemplate>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Dancer-Template-MicroTemplate>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Dancer-Template-MicroTemplate/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2010 Franck Cuny.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
+This module is free software and is released under the same terms as Perl
+itself.
 
 =cut
-
-1; # End of Dancer::Template::MicroTemplate
